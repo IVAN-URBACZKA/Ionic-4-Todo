4b00488d0a93afbdc3f2ff3b898484bca88e4ac1
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2019-12-06 16:05:25 +0100

    finish

--- todo/src/app/tab1/tab1.page.ts
+++ todo/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
@@ -10,26 +11,200 @@ export class Tab1Page {
 
   priorityRef: any;
   priorities: any;
+  goalsRef: any;
+  goals: any;
+  quotesRef: any;
+  quotes: any;
 
-  constructor(public afDB: AngularFireDatabase) 
+  constructor(public afDB: AngularFireDatabase,public alertController: AlertController) 
   {
       this.priorityRef =  this.afDB.list('priorities');
+      this.quotesRef =  this.afDB.list('quotes');
+      this.goalsRef =  this.afDB.list('goals');
+
+      this.getFirebaseData();
+
+    }
+
+    getFirebaseData()
+     {
+
       this.priorityRef.snapshotChanges(['child_changed'])
-    .subscribe(actions => {
-      const data = [];
-        actions.forEach(action => {
-      console.log(action.payload.val().text);
-
-        data.push({
-          key: action.payload.key,
-      text: action.payload.val().text,
-      type: 'priority'
-    });
-    });
-
-  this.priorities = data;
+      .subscribe(actions => {
+        const data = [];
+          actions.forEach(action => {
+        console.log(action.payload.val().text);
   
-    });
+          data.push({
+            key: action.payload.key,
+        text: action.payload.val().text,
+        type: 'priority'
+      });
+      });
+  
+    this.priorities = data;
+    
+      });
+
+      this.goalsRef.snapshotChanges(['child_changed'])
+      .subscribe(actions => {
+        const data = [];
+          actions.forEach(action => {
+        console.log(action.payload.val().text);
+  
+          data.push({
+            key: action.payload.key,
+        text: action.payload.val().text,
+        type: 'goals'
+      });
+      });
+  
+    this.goals = data;
+    
+      });
+
+      this.quotesRef.snapshotChanges(['child_changed'])
+      .subscribe(actions => {
+        const data = [];
+          actions.forEach(action => {
+        console.log(action.payload.val().text);
+  
+          data.push({
+            key: action.payload.key,
+        text: action.payload.val().text,
+        type: 'quotes'
+      });
+      });
+  
+    this.quotes = data;
+    
+      });
+
+    }
+
+    async addPriority() {
+      const alert = await this.alertController.create({
+        header: 'Ajouter votre priorité!',
+        inputs: [
+          {
+            name: 'priority',
+            type: 'text',
+            placeholder: 'Votre priorité'
+          },
+         
+          
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Add',
+            handler: (data) => {
+              console.log(data.priority);
+                this.priorityRef.push({
+                text: data.priority
+              });
+              this.getFirebaseData();
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
+
+    async addGoal() {
+      const alert = await this.alertController.create({
+        header: 'Ajouter votre but!',
+        inputs: [
+          {
+            name: 'goal',
+            type: 'text',
+            placeholder: 'Votre but'
+          },
+         
+          
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Add',
+            handler: (data) => {
+              console.log(data.goal);
+              this.goalsRef.push({
+                text: data.goal
+              });
+              this.getFirebaseData();
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
+
+    async addQuote() {
+      const alert = await this.alertController.create({
+        header: 'Ajouter votre citation!',
+        inputs: [
+          {
+            name: 'quote',
+            type: 'text',
+            placeholder: 'Votre Citation'
+          },
+         
+          
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Add',
+            handler: (data) => {
+              console.log(data.quote);
+              this.quotesRef.push({
+                text: data.quote
+              });
+              this.getFirebaseData();
+            
+            }
+          }
+        ]
+      });
+  
+      await alert.present();
+    }
+
+    delete(item:any)
+    {
+      if(item.type === "priority"){
+        this.priorityRef.remove(item.key);
+      }
+      if(item.type === "goals"){
+        this.goalsRef.remove(item.key);
+      }
+      if(item.type === "quotes"){
+        this.quotesRef.remove(item.key);
+      }
+      this.getFirebaseData();
+
+
     }
 
 }
