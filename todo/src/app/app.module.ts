f279869629a5f210c480f1f5dc04164c66d364ce
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2020-01-26 15:24:21 +0100

    Update app.module.ts

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -11,16 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-  // Your web app's Firebase configuration
-  export const firebaseConfig = {
-    apiKey: "AIzaSyAQsTPs3WX8ONJ0-usaRMr_hF5ZbP00uaY",
-    authDomain: "todolist-2306f.firebaseapp.com",
-    databaseURL: "https://todolist-2306f.firebaseio.com",
-    projectId: "todolist-2306f",
-    storageBucket: "todolist-2306f.appspot.com",
-    messagingSenderId: "1013429867657",
-    appId: "1:1013429867657:web:74b2fb02515f3d62346258"
-  };
+
 
 @NgModule({
   declarations: [AppComponent],
